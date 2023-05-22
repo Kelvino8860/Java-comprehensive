cfbab58278e765f6fb5d7709d8fed6f9f3ccf59d
Kelvino8860 <kelvinnjoroge2001@students.uonbi.ac.ke>
2023-05-22 12:53:16 +0300

    Pick four random cards

--- Chp15/PickFourCards/src/App.java
+++ Chp15/PickFourCards/src/App.java
@@ -1,3 +1,9 @@
+/**
+ * *15.1 (Pick four cards) Write a program that lets the user click the Refresh button to
+ * display four cards from a deck of 52 cards, as shown in Figure 15.26a. (See the
+ * hint in Programming Exercise 14.3 on how to obtain four random cards.)
+ */
+
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
