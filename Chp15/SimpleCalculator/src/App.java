7e7f405d6967b8a88cfd9f7c03f8a1708d25e62d
Kelvino8860 <kelvinnjoroge2001@students.uonbi.ac.ke>
2023-05-25 14:51:46 +0300

    SimpleCalculator

--- Chp15/SimpleCalculator/src/App.java
+++ Chp15/SimpleCalculator/src/App.java
@@ -1,6 +1,10 @@
+/**
+ * (Create a simple calculator) Write a program to perform addition, subtraction,
+ * multiplication, and division, as shown in Figure 15.27a.
+ */
+
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class App extends Application {
